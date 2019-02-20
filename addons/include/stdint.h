30f979eae28e58085a859b727acf7068d315ebe2
Jeroen Doggen <jeroendoggen@gmail.com>
2019-02-20 13:00:33 +0100

    MISRA Addon: add stdint.h

---
+++ addons/include/stdint.h
@@ -0,0 +1,13 @@
+// These standard types are needed for the cppcheck MISRA addon
+typedef unsigned char byte; 
+typedef unsigned short word; 
+typedef unsigned int dword; 
+typedef int bool; 
+typedef signed char int8_t; 
+typedef unsigned char uint8_t; 
+typedef signed short int int16_t; 
+typedef unsigned short int uint16_t; 
+typedef signed int int32_t; 
+typedef unsigned int uint32_t; 
+typedef unsigned long long uint64_t;
+typedef long long int64_t;
